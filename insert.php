9e3d03b3fce6ffda57494cafcaba91a1a50c633f
jimmy mwambatshi <jimmymwambatshi12@gmail.com>
2019-06-24 21:57:23 +0200

    add

--- insert.php
+++ insert.php
@@ -9,7 +9,7 @@ if(isset($_POST["image"]))
  $data = $_POST["image"];
 
  $image_array_1 = explode(";", $data);
-d
+
  $image_array_2 = explode(",", $image_array_1[1]);
 
  $data = base64_decode($image_array_2[1]);
